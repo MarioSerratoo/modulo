7f824d1a708d24a68251e15532b10e741b8cd804
Mario <mario.serrato@uabc.edu.mx>
2022-10-16 10:46:16 -0700

    Creacio de archivos py

---
+++ zen372.py
@@ -0,0 +1,18 @@
+import this
+
+# Comentario en una linea
+"""
+Comentario multilinea
+1
+2
+3
+...
+"""
+#Zen de la clase 372
+print("No se")
+print("Mas vale pedir perdon que pedir permiso")
+print("Camaraon que se duerme se lo lleva la corriente")
+print ("Fuga")
+print("Fuga x 2")
+
+barba = "sorpresa!!!"
